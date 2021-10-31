b91c1e9c7879084b53e8132212447e479c519111
lily.slvr <lily@silverlily.net>
2021-10-31 02:54:43 -0600

    fix: a

--- src/syn.ts
+++ src/syn.ts
@@ -13,3 +13,17 @@ if (settings.devMode) {
 
 let shard = child.fork(`./${env}/discord/shard`);
 let ps2 = child.fork(`./${env}/planetside/eventsTracker`);
+
+shard.on("exit", (code) => {
+  if (code && code != 0) {
+    ps2.kill();
+    process.kill(code);
+  }
+});
+
+ps2.on("exit", (code) => {
+  if (code && code != 0) {
+    shard.kill();
+    process.kill(code);
+  }
+});
